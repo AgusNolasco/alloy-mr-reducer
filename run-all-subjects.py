d0e6756d0810d163a2e3f4583981aad5bf92cc75
AgusNolasco <nolasco@dc.exa.unrc.edu.ar>
2023-01-12 21:46:26 -0300

    Add a run all subject script

---
+++ run-all-subjects.py
@@ -0,0 +1,13 @@
+import os
+import subprocess
+import sys
+
+subject_set  = sys.argv[1]
+gen_strategy = sys.argv[2]
+mrs_to_fuzz  = sys.argv[3]
+allow_epa_loops = sys.argv[4]
+
+subjects_path = f'experiments/{subject_set}-subjects.txt'
+with open(subjects_path) as subjects_file:
+    for subject in subjects_file:
+        result = subprocess.run(f'experiments/run.sh {subject} {gen_strategy} {mrs_to_fuzz} {allow_epa_loops}', shell=True)
