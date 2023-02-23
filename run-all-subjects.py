fbbb11e1ea71340d12abe36d9797519c65b6e164
AgusNolasco <nolasco@dc.exa.unrc.edu.ar>
2023-02-23 20:27:49 -0300

    Now when we run the SAT-reduction for each subject we show the time that it takes

--- run-all-subjects.py
+++ run-all-subjects.py
@@ -10,4 +10,4 @@ allow_epa_loops = sys.argv[4]
 subjects_path = f'experiments/{subject_set}-subjects.txt'
 with open(subjects_path) as subjects_file:
     for subject in subjects_file:
-        result = subprocess.run(f'experiments/run.sh {subject} {gen_strategy} {mrs_to_fuzz} {allow_epa_loops}', shell=True)
+        result = subprocess.run(f'/usr/bin/time -f"%e" experiments/run.sh {subject} {gen_strategy} {mrs_to_fuzz} {allow_epa_loops}', shell=True)
