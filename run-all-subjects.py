f14f47918f9e056ad27554bec23878fff4642674
AgusNolasco <nolasco@dc.exa.unrc.edu.ar>
2023-02-23 22:37:06 -0300

    Update run all subjects script

--- run-all-subjects.py
+++ run-all-subjects.py
@@ -10,4 +10,11 @@ allow_epa_loops = sys.argv[4]
 subjects_path = f'experiments/{subject_set}-subjects.txt'
 with open(subjects_path) as subjects_file:
     for subject in subjects_file:
-        result = subprocess.run(f'/usr/bin/time -f"%e" experiments/run.sh {subject} {gen_strategy} {mrs_to_fuzz} {allow_epa_loops}', shell=True)
+        subject = subject.strip()
+        if '#' in subject:
+            continue
+        print('Running: ' + subject)
+        result = subprocess.run(f'/usr/bin/time -f"%e" experiments/run.sh {subject} {gen_strategy} {mrs_to_fuzz} {allow_epa_loops}', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output = result.stderr.decode('utf-8')
+        values = output.split('\n')
+        print(f'{subject}: {values[-2]}')
