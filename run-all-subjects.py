0f5142dd22d1b84b6fb91ff3105aec725c23d337
AgusNolasco <nolasco@dc.exa.unrc.edu.ar>
2023-08-15 14:16:42 -0300

    Update run all subjects script

--- run-all-subjects.py
+++ run-all-subjects.py
@@ -7,10 +7,13 @@ gen_strategy = sys.argv[2]
 mrs_to_fuzz  = sys.argv[3]
 allow_epa_loops = sys.argv[4]
 
+seeds = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 subjects_path = f'experiments/{subject_set}-subjects.txt'
 with open(subjects_path) as subjects_file:
     for subject in subjects_file:
         subject = subject.strip()
         if '#' in subject:
             continue
-        result = subprocess.run(f'experiments/run.sh {subject} {gen_strategy} {mrs_to_fuzz} {allow_epa_loops}', shell=True)
+        for seed in seeds:
+            result = subprocess.run(f'experiments/run.sh {subject} {gen_strategy} {mrs_to_fuzz} {allow_epa_loops} {seed}', shell=True)
