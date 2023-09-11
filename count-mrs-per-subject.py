40a5c08069562d199becd5c6b3d7d55b27616c8d
AgusNolasco <nolasco@dc.exa.unrc.edu.ar>
2023-09-11 14:26:24 -0300

    Update count mrs script

--- count-mrs-per-subject.py
+++ count-mrs-per-subject.py
@@ -5,6 +5,8 @@ gen_strategy = sys.argv[1]
 mrs_to_fuzz  = sys.argv[2]
 allow_epa_loops = sys.argv[3]
 
+seeds = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 def split_mr(mr):
     precond = mr.split(' -> ')[0]
     precond = precond[1:len(precond)-1]
@@ -21,14 +23,15 @@ def split_mrs(mrs):
     return split_mrs
 
 for subject in os.listdir('output'):
-    path_to_mrs = f'output/{subject}/allow_epa_loops_{allow_epa_loops}/{gen_strategy}/{mrs_to_fuzz}/mrs.txt'
-    if not os.path.isfile(path_to_mrs):
-        continue
-    with open(path_to_mrs) as mrs_file:
-        mrs = [line.rstrip() for line in mrs_file]
-        first_mark = mrs.index('')
-        mrs = mrs[first_mark+1:]
-        second_mark = mrs.index('')
-        mrs = mrs[:second_mark]
-        mrs = split_mrs(mrs)
-        print(f'{subject}: {len(mrs)}')
+    for seed in seeds:
+        path_to_mrs = f'output/{subject}/allow_epa_loops_{allow_epa_loops}/{gen_strategy}/{mrs_to_fuzz}/{seed}/mrs.txt'
+        if not os.path.isfile(path_to_mrs):
+            continue
+        with open(path_to_mrs) as mrs_file:
+            mrs = [line.rstrip() for line in mrs_file]
+            first_mark = mrs.index('')
+            mrs = mrs[first_mark+1:]
+            second_mark = mrs.index('')
+            mrs = mrs[:second_mark]
+            mrs = split_mrs(mrs)
+            print(f'{subject} - seed {seed}: {len(mrs)}')
